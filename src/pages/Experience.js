6272d84c41c3db9c1bd915e74455f0cd4f7173c0
Jelili Abdullahi A <osuolaleabdullahi@gmail.com>
2022-03-30 11:31:29 +0100

    Experience added

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,19 +1,31 @@
 import React from "react";
-import ExperienceList from "../components/experience/ExperienceList";
-import SectionHeader from "../components/global/SectionHeader";
+// import ExperienceList from "../components/experience/ExperienceList";
+import Card, { CardContent } from "../components/global/Card";
+// import SectionHeader from "../components/global/SectionHeader";
 import "./Experience.css";
 
 function Experience() {
   return (
-    <section className="experience">
-      <SectionHeader>
+    <Card className="experience">
+      {/* <SectionHeader>
         <p>WORK EXPERIENCE</p>
 
         <h2>Companies I have worked for in the past.</h2>
       </SectionHeader>
 
-      <ExperienceList />
-    </section>
+      <ExperienceList /> */}
+      <CardContent className="experience__content">
+        <h2>Personal Experience</h2>
+
+        <p>
+          Up until 2009, I was a medical doctor before switching to investment
+          management.
+        </p>
+
+        <a href="#read-more">Read more</a>
+      </CardContent>
+      <div className="experience__img"></div>
+    </Card>
   );
 }
 
