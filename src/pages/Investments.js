514b1bc27c11735238607f26753d39fb2990ba4f
Jelili Abdullahi A <osuolaleabdullahi@gmail.com>
2022-03-30 22:12:34 +0100

    Svg Icons added

--- src/pages/Investments.js
+++ src/pages/Investments.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Investments.css";
+import Fade from "react-reveal/Fade";
+import { MdAcUnit } from "react-icons/md";
+import { IoDuplicateOutline } from "react-icons/io5";
+import { GiBrainTentacle } from "react-icons/gi";
+import { SiFastlane } from "react-icons/si";
 
 function Investments() {
   return (
@@ -15,42 +20,51 @@ function Investments() {
         </p>
       </div>
 
-      <div className="investments__item">
-        <span>SVG ICON</span>
+      <Fade delay={100}>
+        <div className="investments__item">
+          <MdAcUnit />
 
-        <h3>Uniqueness</h3>
+          <h3>Uniqueness</h3>
 
-        <p>
-          The core of every investment thesis is based on the uniqueness of the
-          business solution
-        </p>
-      </div>
+          <p>
+            The core of every investment thesis is based on the uniqueness of
+            the business solution
+          </p>
+        </div>
+      </Fade>
 
-      <div className="investments__item">
-        <span>SVG ICON</span>
+      <Fade delay={100}>
+        <div className="investments__item">
+          <GiBrainTentacle />
 
-        <h3>Innovation</h3>
+          <h3>Innovation</h3>
 
-        <p>I look out for innovative solutions across diverse industries</p>
-      </div>
+          <p>I look out for innovative solutions across diverse industries</p>
+        </div>
+      </Fade>
 
-      <div className="investments__item">
-        <span>SVG ICON</span>
+      <Fade delay={100}>
+        <div className="investments__item">
+          <SiFastlane />
 
-        <h3>Competitive Advantage</h3>
+          <h3>Competitive Advantage</h3>
 
-        <p>I look out for something around a blue ocean strategy</p>
-      </div>
+          <p>I look out for something around a blue ocean strategy</p>
+        </div>
+      </Fade>
 
-      <div className="investments__item">
-        <span>SVG ICON</span>
+      <Fade delay={100}>
+        <div className="investments__item">
+          <IoDuplicateOutline />
 
-        <h3>Imitations and Duplication</h3>
+          <h3>Imitations and Duplication</h3>
 
-        <p>
-          I avoid duplication of ideas; in layman terms, I don’t like copycats.
-        </p>
-      </div>
+          <p>
+            I avoid duplication of ideas; in layman terms, I don’t like
+            copycats.
+          </p>
+        </div>
+      </Fade>
     </section>
   );
 }
